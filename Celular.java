98a7cc209f0994bf51fdc2b2a0fa5ef719924566
Anderson Mário Neto <122921063+andersonmarioneto@users.noreply.github.com>
2024-04-08 00:05:21 +0100

    Actulizacao

--- Celular.java
+++
@@ -1,7 +0,0 @@
-public class Celular {
-    // ATRIBUTOS = Caracteristicas 
-    String nome; // atributo chamado nome de tipo String
-    String sistemaOperacional; 
-    int espacoArmazenamento; 
-    float tamanhoTela;
-}
